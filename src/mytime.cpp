9056ac03d1d2cd3e1547165e6a660e3d01b1c0f8
Kloenk <kloenk@kloenk.de>
2019-07-18 22:15:27 +0200

    WIP: trie time epoch conversion

--- src/mytime.cpp
+++ src/mytime.cpp
@@ -23,10 +23,39 @@ String mytime::time() {
 char *mytime::isoTime() {
     timeClient.update();
 
-    //tm* tmS = localtime((time_t*)timeClient.getEpochTime());
-    tm* tmS = gmtime((time_t*)timeClient.getEpochTime()- 946684800);
+    long epoch = timeClient.getEpochTime();
+
+    byte second = epoch%60; epoch /= 60;
+    byte minute = epoch%60; epoch /= 60;
+    byte hour   = epoch%24; epoch /= 24;
+
+    unsigned int years = epoch/(365*4+1)*4; epoch %= 365*4+1;
+    unsigned int year;
+    for (year=3; year>0; year--)
+    {
+        if (epoch >= days[year][0])
+            break;
+    }
+
+    unsigned int month;
+    for (month=11; month>0; month--)
+    {
+        if (epoch >= days[year][month])
+            break;
+    }
+
+    year  = years+year;
+    month = month+1;
+    unsigned int day   = epoch - days[year][month]+1;
+
+    unsigned int weekday  = (dayOfMonth += month < 3 ? year-- : year - 2, 23*month/9 + dayOfMonth + 4 + year/4- year/100 + year/400)%7;
 
 
-    //char* iso = asctime(tmS);	
-    return "iso";
+    //char* iso = asctime(tmS);
+    return sprintf("%s%s%sUTC%s",
+      String(day).c_str(),
+      String(hour).c_str(),
+      String(minute).c_str(),
+      String(month).c_str(),
+    );
 }
